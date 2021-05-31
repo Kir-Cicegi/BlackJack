ec8f557a496ffaa475573e4aa301ea051677af50
Kir-Cicegi <84298571+Kir-Cicegi@users.noreply.github.com>
2021-05-31 15:18:55 -0400

    Got a random card to show up for the player

--- script.js
+++ script.js
@@ -11,49 +11,61 @@ let bStart = document.getElementById("start")
 let bHit = document.getElementById("hit")
 let bStand = document.getElementById("start")
 let bInfo = document.getElementById("info")
+let house = document.getElementById("house")
 
+
+
+
+// bStart.addEventListener("click", pickCard)
 
 
 //create a deck of cards
 
-let suits = ["H", "C", "D", "S"]
-let ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+let suits = ["h", "c", "d", "s"]
+let ranks = ["02", "03", "04", "05", "06", "07", "08", "09", "10", "J", "Q", "K", "A"]
 
 let deck = []
 
 for(let i = 0; i<suits.length; i++){
     for(let j=0; j<ranks.length; j++){
         let value = parseInt(ranks[j]);//number value of each array item of ranks
-        console.log(ranks[j])
         if (ranks[j] === "A")  {
             value = 11;
         }
         if(ranks[j] === "J" || ranks[j] === "Q" || ranks[j] === "K") {
         value = 10;
         }
-    deck.push({Suit: suits[i], Rank: ranks[j], Value: value});     
+        let eachCard = (suits[i]+ranks[j])
+        console.log(eachCard)
+    let individualCard= {Suit: suits[i], Rank: ranks[j], Value: value, Class: eachCard};  
+
+    deck.push(individualCard);
     }
 }
 console.log(deck)
 
-//use the same method I used for translator to get the values of each card from the object (.map())
+// how to make cards appear!!!!???
 
 //make a function to randomly pick from the deck. 
 
-let randomCard = deck[Math.floor(Math.random() * deck.length)]
+function pickCard (){   
+let randomCard = [deck[Math.floor(Math.random() * deck.length)]]
 console.log(randomCard)
-
-function randomPicks (){
-    
+let newClass = document.createElement("DIV")
+let result = randomCard.map(a => a.Class)
+console.log(result)
+newClass.className= "card "+result;
+console.log(newClass)
+player.appendChild(newClass);
 }
 
 
+pickCard();
 
 
 //start button initializes the game. 
 //deals two random cards to both 
 //updates the count with sum of two values 
-
 
 
 
